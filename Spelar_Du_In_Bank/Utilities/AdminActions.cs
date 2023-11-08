cc40852961cc95f05f3e2cd64ee169bf6cbc8c4b
lucine1029 <sherry.jing@hotmail.com>
2023-11-08 15:39:56 +0100

    Move AdminActions under Utitlities folder

--- Spelar_Du_In_Bank/Utilities/AdminActions.cs
+++ Spelar_Du_In_Bank/Utilities/AdminActions.cs
@@ -1,13 +1,12 @@
 ﻿using Spelar_Du_In_Bank.Data;
 using Spelar_Du_In_Bank.Model;
-using Spelar_Du_In_Bank.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Spelar_Du_In_Bank
+namespace Spelar_Du_In_Bank.Utilities
 {
     internal static class AdminActions
     {
