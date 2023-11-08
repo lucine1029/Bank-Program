e435e8853def8fb927fc7a02ad6a0764f98ade49
lucine1029 <sherry.jing@hotmail.com>
2023-11-08 15:20:27 +0100

    Copy from Kristoffer and build up our draft

--- Spelar_Du_In_Bank/Model/User.cs
+++ Spelar_Du_In_Bank/Model/User.cs
@@ -12,9 +12,9 @@ namespace Spelar_Du_In_Bank.Model
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Pin { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
-        public string SSN { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? SSN { get; set; }
 
         public virtual ICollection<Account> Accounts { get; set; }
     }
---
+++ Spelar_Du_In_Bank/Utilities/DbHelper.cs
@@ -0,0 +1,34 @@
+﻿using Spelar_Du_In_Bank.Data;
+using Spelar_Du_In_Bank.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spelar_Du_In_Bank.Utilities
+{
+    internal static class DbHelper
+    {
+        public static List<User> GetAllUsers(BankContext context)
+        {
+            List<User> users = context.Users.ToList();
+            return users;
+        }
+
+        public static bool AddUser(BankContext context, User user)
+        {
+            context.Users.Add(user);
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error adding user: {e}");
+                return false;
+            }
+            return true;
+        }
+    }
+}
