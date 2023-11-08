84ebc2e2cbacba572a03068f789a267755560155
mmxzps <mojtaba.mobasheri@hotmail.com>
2023-11-08 15:46:48 +0100

    Added MenuAction class

---
+++ Spelar_Du_In_Bank/Utilities/MenuAction.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spelar_Du_In_Bank.Utilities
+{
+    internal class MenuAction
+    {
+        //1. Se dina konton och saldo
+        //2. Överföring mellan konton
+        //3. Ta ut pengar
+        //4. Sätt in pengar
+        //5. Öppna nytt konto
+        //6. Logga ut
+    }
+}
