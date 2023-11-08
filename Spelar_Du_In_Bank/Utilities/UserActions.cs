94435744285acaf81b4890dab126106a6f6194fd
jonnytouma <jonnytouma@gmail.com>
2023-11-08 15:42:08 +0100

    created userfunction class
    Added assignments

---
+++ Spelar_Du_In_Bank/Utilities/UserActions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spelar_Du_In_Bank.Utilities
+{
+    internal static class UserActions
+    {
+        /*
+          Se dina konton och saldo	-Max
+          Överföring mellan konton	-Jing
+          Ta ut pengar	-Sean
+          Sätt in pengar	-Muhtaba
+          Öppna nytt konto	-Jonny
+         */
+    }
+}
